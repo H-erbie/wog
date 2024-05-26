19172deeef54268c5632fb1dda508808033c1a8d
H-erbie <iamkorantengansongherbert@gmail.com>
2024-05-26 17:21:11 +0100

    t14

--- app/(user_guest)/about/page.jsx
+++ app/(user_guest)/about/page.jsx
@@ -30,17 +30,17 @@ const Page = async() => {
 
     <div class="container mx-auto px-4 py-8">
   <h1 class="text-2xl font-bold mb-4">Andamo Express: Your Speedy Shopping Shortcut</h1>
-  <p class="text-lg mb-8">Ever get that "gotta-have-it-now" feeling and need it fast? That's where Andamo Express comes in! We're your one-stop shop for a huge variety of products, delivered straight to your door at lightning speed.</p>
+  <p class="text-lg mb-8">Ever get that &apos;gotta-have-it-now&apos; feeling and need it fast? That&apos;s where Andamo Express comes in! We&apos;re your one-stop shop for a huge variety of products, delivered straight to your door at lightning speed.</p>
 
   <div class="flex items-center mb-8">
     {/* <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 mr-4 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7 -7 7 7" />
     </svg> */}
-    <p class="text-lg">Born from a simple idea: online shopping should be convenient, affordable, and hassle-free. That's why we built Andamo Express as a Progressive Web App (PWA), giving you a smooth shopping experience right from your phone or mobile device.</p>
+    <p class="text-lg">Born from a simple idea: online shopping should be convenient, affordable, and hassle-free. That&apos;s why we built Andamo Express as a Progressive Web App (PWA), giving you a smooth shopping experience right from your phone or mobile device.</p>
   </div>
 
-  <h2 class="text-xl font-bold mb-4">But we're more than just fast delivery</h2>
-  <p class="text-lg mb-8">We're passionate about bringing you the best possible selection of products, from top brands you trust to exciting new discoveries. Our friendly customer service team is always here to help, and we offer secure checkout and easy returns for total peace of mind.</p>
+  <h2 class="text-xl font-bold mb-4">But we&apos;re more than just fast delivery</h2>
+  <p class="text-lg mb-8">We&apos;re passionate about bringing you the best possible selection of products, from top brands you trust to exciting new discoveries. Our friendly customer service team is always here to help, and we offer secure checkout and easy returns for total peace of mind.</p>
 
   <h3 class="text-lg font-bold mb-4">The Andamo Express Team:</h3>
   <div class="">
@@ -68,7 +68,7 @@ const Page = async() => {
   
   </div>
 
-  <p class="text-lg mt-8 text-center">We're more than just an online store, we're your partner in getting what you need, fast! So why wait? Start shopping at Andamo Express today!</p>
+  <p class="text-lg mt-8 text-center">We&apos;re more than just an online store, we&apos;re your partner in getting what you need, fast! So why wait? Start shopping at Andamo Express today!</p>
 
   <Link href='/' class="mx-auto bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 mt-4 block w-max">Shop Now</Link>
 </div>
