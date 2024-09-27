5f88dda15b4be1f91c0067faf2baf95dfd5ab48f
Alfred Asare-Appiah <iamkorantengansongherbert@gmail.com>
2024-09-27 02:03:58 +0000

    new8

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          // Configure babel presets and plugins here
+        },
+      },
+    });
+    return config;
+  },
     images: {
         remotePatterns: [
           {
