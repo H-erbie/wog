ffb0a17219afea185c0bcaa016d0f9fbcf28ece3
Alfred Asare-Appiah <iamkorantengansongherbert@gmail.com>
2024-09-27 02:24:42 +0000

    new11

--- app/(private)/layout.jsx
+++ app/(private)/layout.jsx
@@ -1,16 +1,16 @@
 'use client'
 import React,{useEffect} from "react";
 import {redirect} from 'next/navigation'
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/config";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth } from "@/firebase/config";
 
 export default  function PrivateLayout ({children}){
     const isUserDataStored = JSON.parse(sessionStorage.getItem("wog-user"))
-    const [user] = useAuthState(auth);
+    // const [user] = useAuthState(auth);
 
-    useEffect(()=> {
-        if((!user && isUserDataStored) || !isUserDataStored ) redirect('/auth/sign-in')
-    },[isUserDataStored, user])
+    // useEffect(()=> {
+    //     if((!user && isUserDataStored) || !isUserDataStored ) redirect('/auth/sign-in')
+    // },[isUserDataStored, user])
 
     return <>{children}</>
 }
